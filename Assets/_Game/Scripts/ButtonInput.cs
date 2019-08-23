7107aa0f418da1c25c428cb5e993d92e4c1a2d4b
Pavel Barros Quintanilla <pavelbarrosquintanilla@Pavels-MacBook-Pro.local>
2019-08-23 14:10:16 +0200

    First commit on ParachuteGame. Added startplace + movement

---
+++ Assets/_Game/Scripts/ButtonInput.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonInput : MonoBehaviour
+{
+    public bool left;
+    public delegate void ButtonPressed();
+    public static event ButtonPressed OnLeft;
+    public static event ButtonPressed OnRight;
+
+
+    private void OnMouseDown()
+    {
+        if (OnLeft != null && left)
+        {
+            OnLeft();
+            Debug.Log("Left Pressed!");
+        }
+        else if (OnRight != null)
+        {
+            OnRight();
+            Debug.Log("Right Pressed!");
+        }
+    }
+}
---
+++ Assets/_Game/Scripts/RescuerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RescuerController : MonoBehaviour
+{
+    public List<Transform> movementPoints = new List<Transform>();
+    private int currentPosition = 1;
+
+    private void OnEnable()
+    {
+        ButtonInput.OnLeft += OnLeftPressed;
+        ButtonInput.OnRight += OnRightPressed;
+    }
+
+    private void OnDisable()
+    {
+        ButtonInput.OnLeft -= OnLeftPressed;
+        ButtonInput.OnRight -= OnRightPressed;
+    }
+
+
+    private void Start()
+    {
+        UpdatePosition();
+    }
+
+
+    private void OnLeftPressed()
+    {
+        if (currentPosition > 0)
+        {
+            currentPosition--;
+            UpdatePosition();
+            //Debug.Log("Moved Left");
+        }
+    }
+
+
+    private void OnRightPressed()
+    {
+        if (currentPosition < movementPoints.Count - 1)
+        {
+            currentPosition++;
+            UpdatePosition();
+            //Debug.Log("Moved Right");
+        }
+    }
+
+
+    private void UpdatePosition()
+    {
+        transform.position = movementPoints[currentPosition].position;
+    }
+
+}
