76e97ed1c6a6f0b88a807288f080dcd75a6f7acd
Pavel Barros Quintanilla <pavelbarrosquintanilla@Pavels-MacBook-Pro.local>
2019-09-02 15:56:31 +0200

    Added prefab, spawnPoints, Still bugs.

---
+++ Assets/_Game/Scripts/JumperController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumperController : MonoBehaviour
+{
+    [SerializeField]
+
+    private List<Transform> positions = new List<Transform>();
+
+    public int currentPosition = 0;
+
+    float lastMoveTime;
+    float moveDelay = 1.0f;
+
+    private void Start()
+    {
+        transform.position = positions[currentPosition].position;
+        lastMoveTime = Time.time;
+    }
+
+    IEnumerator Move()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(moveDelay);
+            MoveToNextPosition();
+        }
+    }
+
+    void MoveToNextPosition()
+    {
+
+        currentPosition++;
+
+        if (currentPosition >= positions.Count)
+        {
+            GameObject parent = transform.parent.gameObject;
+            Destroy(parent);
+        } else {
+
+            transform.position = positions[currentPosition].position;
+
+        }
+
+    }
+
+
+}
---
+++ Assets/_Game/Scripts/JumperSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumperSpawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject jumperPrefab;
+
+    float lastSpawnTime;
+
+    [Range(0, 5)]
+    public float spawnDelay = 3.0f;
+    [Range(0, 2)]
+    public float deltaRandomSpawn = 0.5f;
+
+
+    public float randomSpawnDelay;
+
+    private void Start()
+    {
+        if (jumperPrefab == null)
+            return;
+
+        randomSpawnDelay = spawnDelay;
+        SpawnJumper();
+    }
+
+    private void Update()
+    {
+        if (Time.time > lastSpawnTime + randomSpawnDelay)
+        {
+            SpawnJumper();
+        }
+    }
+
+
+    private void SpawnJumper()
+    {
+        lastSpawnTime = Time.time;
+        randomSpawnDelay = Random.Range(spawnDelay - deltaRandomSpawn, spawnDelay + deltaRandomSpawn);
+        Instantiate(jumperPrefab);
+    }
+}
