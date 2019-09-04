238269f0cc49e5489095c5f06caff0a54bcbf7d6
Pavel Barros Quintanilla <pavelbarrosquintanilla@Pavels-MacBook-Pro.local>
2019-09-04 13:51:24 +0200

    Added 3 paths for jumpers

--- Assets/_Game/Scripts/JumperController.cs
+++ Assets/_Game/Scripts/JumperController.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class JumperController : MonoBehaviour
 {
     [SerializeField]
-
-    private List<Transform> positions = new List<Transform>();
+    private List<Transform> leftPositions = new List<Transform>();
+    [SerializeField]
+    private List<Transform> midPositions = new List<Transform>();
+    [SerializeField]
+    private List<Transform> rightPositions = new List<Transform>();
 
     public int currentPosition = 0;
 
+    private List<Transform> pos;
+
     float lastMoveTime;
     float moveDelay = 1.0f;
+    int rand;
 
     private void Start()
     {
-        transform.position = positions[currentPosition].position;
+        rand = Random.Range(1, 4);
+        transform.position = leftPositions[currentPosition].position;
         lastMoveTime = Time.time;
         StartCoroutine(Move());
     }
@@ -25,25 +32,40 @@ public class JumperController : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(moveDelay);
-            MoveToNextPosition();
+            RandomPaths();
         }
     }
 
-    void MoveToNextPosition()
+    void MoveToNextPosition(List<Transform> position)
     {
 
         currentPosition++;
 
-        if (currentPosition >= positions.Count)
+        if (currentPosition >= position.Count)
         {
             GameObject parent = transform.parent.gameObject;
             Destroy(parent);
         } else {
 
-            transform.position = positions[currentPosition].position;
+            transform.position = position[currentPosition].position;
 
         }
 
+    }
+
+    private void RandomPaths()
+    {
+        switch (rand)
+        {
+            case 1: MoveToNextPosition(leftPositions);
+                break;
+
+            case 2: MoveToNextPosition(midPositions);
+                break;
+
+            case 3: MoveToNextPosition(rightPositions);
+                break;
+        }
     }
 
 
